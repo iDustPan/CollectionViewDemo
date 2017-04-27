ec02d2438c35d15aae411cbaa7a96a48cf8a39dd
Pan <pan@borderxlab.com>
2017-04-27 17:55:30 +0800

    New project

---
+++ AwsomeCollectionView/AwsomeCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AwsomeCollectionView
+//
+//  Created by borderx_Pan on 2017/4/27.
+//  Copyright © 2017年 borderx_Pan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
+
---
+++ AwsomeCollectionView/AwsomeCollectionViewTests/AwsomeCollectionViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  AwsomeCollectionViewTests.swift
+//  AwsomeCollectionViewTests
+//
+//  Created by borderx_Pan on 2017/4/27.
+//  Copyright © 2017年 borderx_Pan. All rights reserved.
+//
+
+import XCTest
+@testable import AwsomeCollectionView
+
+class AwsomeCollectionViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
